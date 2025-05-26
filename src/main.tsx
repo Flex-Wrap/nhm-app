d91478d0a86331bfe5935038eb24851fc74c3aa6
Aaron Becerril Sanchez <noraasehcnas@gmail.com>
2025-05-26 09:42:57 +0200

    more / mistakes

--- src/main.tsx
+++ src/main.tsx
@@ -6,7 +6,7 @@ import { App } from './App';
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <BrowserRouter basename="/NHM-Solution/">
+    <BrowserRouter basename="/NHM-Solution">
       <App />
     </BrowserRouter>
   </StrictMode>
