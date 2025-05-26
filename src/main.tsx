312f91fc4418f1dbdc1ad4f918b01d30fed67632
Aaron Becerril Sanchez <noraasehcnas@gmail.com>
2025-05-26 09:39:35 +0200

    oops Solution with capital

--- src/main.tsx
+++ src/main.tsx
@@ -6,7 +6,7 @@ import { App } from './App';
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <BrowserRouter basename="/NHM-solution/">
+    <BrowserRouter basename="/NHM-Solution/">
       <App />
     </BrowserRouter>
   </StrictMode>
