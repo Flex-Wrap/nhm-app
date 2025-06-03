0212b660df16d47b212f9d8514d0a990d2c03123
Aaron Becerril Sanchez <noraasehcnas@gmail.com>
2025-06-03 09:40:42 +0200

    exhibitions

---
+++ src/components/ExhibitionsCutout.tsx
@@ -0,0 +1,13 @@
+import './ExhibitionsCutout.css';
+
+const ExhibitionsCutout = () => {
+  return (
+    <div className="card-container">
+      <div className="card card-red" />
+      <div className="card card-green" />
+      <div className="card card-blue" />
+    </div>
+  );
+};
+
+export default ExhibitionsCutout;
--- src/pages/Exhibitions.tsx
+++ src/pages/Exhibitions.tsx
@@ -1,10 +1,10 @@
 import React from 'react';
+import ExhibitionsCutout from '../components/ExhibitionsCutout';
 
 export const Exhibitions: React.FC = () => {
     return (
-        <div>
-            <h1>Exhibitions</h1>
-            <p>Welcome to the Exhibitions page!</p>
-        </div>
+        <>
+        <ExhibitionsCutout />
+        </>
     );
 };
