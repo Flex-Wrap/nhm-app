6750c86076df86afb48420b59e3d9ce95792acf1
Aaron Becerril Sanchez <noraasehcnas@gmail.com>
2025-06-03 11:33:16 +0200

    cutout done

--- src/components/ExhibitionsCutout.tsx
+++ src/components/ExhibitionsCutout.tsx
@@ -1,11 +1,46 @@
+import React from 'react';
 import './ExhibitionsCutout.css';
 
-const ExhibitionsCutout = () => {
+interface CardContent {
+  title: string;
+  picture: string;
+}
+
+interface ExhibitionsCutoutProps {
+  className?: string;
+  left: CardContent;
+  right: CardContent;
+  bottom: CardContent;
+}
+
+const ExhibitionsCutout: React.FC<ExhibitionsCutoutProps> = ({
+  className = '',
+  left,
+  right,
+  bottom
+}) => {
   return (
-    <div className="card-container">
-      <div className="card card-red" />
-      <div className="card card-green" />
-      <div className="card card-blue" />
+    <div className={`card-container ${className}`}>
+      <div
+        className="card card-left"
+        style={{ '--card-image': `url(${left.picture})` } as React.CSSProperties}
+      >
+        <h3 className="card-title">{left.title}</h3>
+      </div>
+
+      <div
+        className="card card-right"
+        style={{ '--card-image': `url(${right.picture})` } as React.CSSProperties}
+      >
+        <h3 className="card-title">{right.title}</h3>
+      </div>
+
+      <div
+        className="card card-bottom"
+        style={{ '--card-image': `url(${bottom.picture})` } as React.CSSProperties}
+      >
+        <h3 className="card-title">{bottom.title}</h3>
+      </div>
     </div>
   );
 };
--- src/pages/Exhibitions.tsx
+++ src/pages/Exhibitions.tsx
@@ -1,10 +1,17 @@
-import React from 'react';
-import ExhibitionsCutout from '../components/ExhibitionsCutout';
+import React from "react";
+import ExhibitionsCutout from "../components/ExhibitionsCutout";
+import leftImg from "../assets/oops.jpg";
+import rightImg from "../assets/beetles.jpg";
+import bottomImg from "../assets/backyard.jpg";
 
 export const Exhibitions: React.FC = () => {
-    return (
-        <>
-        <ExhibitionsCutout />
-        </>
-    );
+  return (
+    <div className="scrollable-container content">
+      <ExhibitionsCutout
+      left={{ title: 'Left Title', picture: leftImg }}
+      right={{ title: 'Right Title', picture: rightImg }}
+      bottom={{ title: 'Bottom Title', picture: bottomImg }}
+    />
+    </div>
+  );
 };
