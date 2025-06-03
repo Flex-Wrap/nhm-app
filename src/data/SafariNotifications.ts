99b29345bc6128e9ba7985ccccebc09734d56315
dia-avv <eaa24avv@students.eaaa.dk>
2025-06-03 14:05:45 +0200

    normal and notifications scenes data is up

---
+++ src/data/SafariNotifications.ts
@@ -0,0 +1,58 @@
+import notification1 from "../assets/notification1.jpg";
+import notification2 from "../assets/notification2.jpg";
+import notification3 from "../assets/notification3.jpg";
+import notification4 from "../assets/notification4.jpg";
+
+export type Notification = {
+  id: number;
+  image: string;
+  header: string;
+  text: string;
+  nextPage: string;
+  previousPage: string;
+  buttonText: string;
+  progress: string; 
+};
+
+export const notifications = [
+  {
+    id: 1,
+    image: notification1,
+    header: "You feel stressed!",
+    text: "Make sure you got everything.",
+    nextPage: "/safariPages/checkbox",
+    previousPage: "/safariPages/scene/2",
+    buttonText: "Continue",
+    progress: "15%"
+  },
+  {
+    id: 2,
+    image: notification2,
+    header: "You are really interested!",
+    text: "Check Jokobsen’s notebook that lay on the table",
+    nextPage: "/safariPages/scene/11",
+    previousPage: "/safariPages/scene/10", // or "safariPages/scene/8",
+    buttonText: "I checked it",
+    progress: "59%"
+  },
+  {
+    id: 3,
+    image: notification3,
+    header: "A new day begins.",
+    text: "Jakobsen is busy with taxidermy",
+    nextPage: "/safariPages/scene/14", 
+    previousPage: "/safariPages/poll-results", 
+    buttonText: "Continue",
+    progress: "86%"
+  },
+  {
+    id: 4,
+    image: notification4,
+    header: "They're not just bones.",
+    text: "Step close - see what they reveal.",
+    nextPage: "/safariPages/scene/16",
+    previousPage: "/safariPages/scene/15",
+    buttonText: "Continue",
+    progress: "97%"
+  }
+];
--- src/data/SafariScenes.ts
+++ src/data/SafariScenes.ts
@@ -9,15 +9,28 @@ import scene78910 from "../assets/scene78910-bg.jpg";
 import scene11 from "../assets/scene11-bg.jpg";
 import scene12 from "../assets/scene12-bg.jpg";
 import scene13 from "../assets/scene13-bg.jpg";
-import scene14 from "../assets/scene14-bg.jpg";
-import scene15 from "../assets/scene15-bg.jpg";
+import scene1415 from "../assets/scene14-bg.jpg";
+import scene16 from "../assets/scene16-bg.jpg";
 
-const scenes = [
+export type Scene = {
+    id: number;
+    text: string;
+    image: string;
+    audio: string;
+    progress: string;
+    nextPage: string;
+    previousPage: string;
+    characterImage: string;
+    buttonText: string;
+}
+
+
+export const scenes = [
     {
         id: 1,
         text: "It’s 1947, a group of explorers from Denmark is preparing to go for an expedition to Africa - not for gold or glory, but to hunt and bring back wild animals for the museum.\n\nKnud Elmdahl -  a well-known nature photographer picked to document the expedition in films and pictures.",
-        image: "/",
-        audio: "/",
+        image: "",
+        audio: "",
         progress: "5%",
         nextPage: "/safariPages/scene/2",
         previousPage: "/safariPages/slider",
@@ -28,7 +41,7 @@ const scenes = [
         id: 2,
         text:"I remember this stop like it was yesterday. The unexpectedly warm wood, the partial shadow under the roof, the scent of dust and earth lingering in the air. I've sat at countless train stops back home in Denmark, yet something about this place felt strangely unfamiliar, like uncharted territory. It brought back memories of packing in a rush.\n\nDid I forget anything? I know my camera is here, the most essential thing. It's so warm, I could probably manage with just the camera if needed. Still, I should probably double-check my suitcase, just to be sure.",
         image:scene2,
-        audio: "/",
+        audio: "",
         progress: "10%",
         nextPage: "/safariPages/notification/1",
         previousPage: "/safariPages/scene/1",
@@ -39,7 +52,7 @@ const scenes = [
         id: 3,
         text: "Hey Knud! The cars are here! Hurry — we don’t wanna be late for camping. I wanna unpack my weapons and get set up. It’s important to get a win on the first day — sets the mood, you know? Brings good luck.",
         image: scene3,
-        audio: "/",
+        audio: "",
         progress: "24%",
         nextPage: "/safariPages/scene/4",
         previousPage: "/safariPages/checkbox",
@@ -50,7 +63,7 @@ const scenes = [
         id: 4,
         text: "At that moment, I still didn’t quite understand what Jakobsen meant by ‘wins’.\n\nI came here to capture beauty — or at least that’s what I told myself. The landscapes… the animals… the people. But there’s a line between capturing and taking, isn’t there?...\n\nSometimes I think the camera did more damage than the bullets. We told ourselves it was for science, for adventure, for pride.\n\nYet my photos feel vain.\n\nI’d like to believe these images serve a purpose — that by trapping those moments of cruelty inside the four black walls of my film, they stay there. Frozen.",
         image: scene4,
-        audio: "/",
+        audio: "",
         progress: "29%",
         nextPage: "/safariPages/scene/5",
         previousPage: "/safariPages/scene/3",
@@ -61,7 +74,7 @@ const scenes = [
         id: 5,
         text: "But I’m here to tell a story — not get lost in my own thoughts.\n\nWhich brings me to the first friend I met on the expedition: the leopard turtle.\n\nLook at it.\n\nIts shell, its carapace...almost identical to our helmets. Same curve. Same purpose. Protection. Survival. They retreat when danger comes — and so do we. Desperately trying to set up camp before nightfall. We blend into the terrain, we wait, we hope not to be seen.\n\nAre we really so different?",
         image: scene5,
-        audio: "/",
+        audio: "",
         progress: "35%",
         nextPage: "/safariPages/scene/6",
         previousPage: "/safariPages/scene/4",
@@ -72,7 +85,7 @@ const scenes = [
         id: 6,
         text: "Look. These are gazelle horns.\n\nThey’re easier to hunt than you'd think. They live in groups—mostly females and their young. One shot, and the whole herd panics. Easy to pick off a second one if you’re fast. You don’t aim for the biggest. You aim for the slowest. The weakest. That’s how it works out here.\n\nPeople get soft when they talk about animals. But it’s just instinct. Predator, prey. That’s the whole game.",
         image: scene6,  
-        audio: "/", 
+        audio: "", 
         progress: "38%",
         nextPage: "/safariPages/choose",
         previousPage: "/safariPages/scene/5",
@@ -83,7 +96,7 @@ const scenes = [
         id: 7,
         text: "So, Jakobsen, do you have family? Kids?",
         image: scene78910,
-        audio: "/",
+        audio: "",
         progress: "48%",
         nextPage: "/safariPages/scene/8",
         previousPage: "/safariPages/choose",
@@ -94,7 +107,7 @@ const scenes = [
         id: 8,
         text: "That’s not your business.\n\nOut here, personal things get people killed. So drop it. Let’s better be back to work. There is my notebook with all the pictures that I took.",
         image: scene78910,
-        audio: "/",
+        audio: "",
         progress: "52%",
         nextPage: "/safariPages/notification/2",
         previousPage: "/safariPages/scene/7",
@@ -105,7 +118,7 @@ const scenes = [
         id: 9,
         text: "Is it your notebook on the table?",
         image: scene78910,
-        audio: "/",
+        audio: "",
         progress: "48%",
         nextPage: "/safariPages/scene/10",
         previousPage: "/safariPages/choose",
@@ -116,7 +129,7 @@ const scenes = [
         id: 10,
         text: "Yes, I draw this pictures I want to catch all of it to not forget important details.",
         image: scene78910,
-        audio: "/",
+        audio: "",
         progress: "52%",
         nextPage: "/safariPages/notification/2",
         previousPage: "/safariPages/scene/9",
@@ -127,7 +140,7 @@ const scenes = [
         id: 11,
         text: "We need to capture all real scenes with animals—every detail, every instinct—to represent it perfectly in Denmark.\n\nIt’s not just about the hunt; it’s about understanding the behavior, the patterns.\n\nThat’s how you make it real.",
         image: scene11,
-        audio: "/",
+        audio: "",
         progress: "62%",
         nextPage: "/safariPages/AR/1",
         previousPage: "/safariPages/scene/10",
@@ -138,7 +151,7 @@ const scenes = [
         id: 12,
         text: "You see, Knud, the savannah's a fascinating place. It’s all about the rhythms of migration. Every year, like clockwork, these plant-eaters—gazelles, zebras, wildebeests—start moving, following the rains—always on the run, looking over their shoulder.\n\nIt’s a harsh life, governed by the hunt. The predators—they’re relentless. But that's nature. The brutality, the survival, it’s normal here. You don’t question it—you just do it...\n\nSpeak of the devil! The lions, let’s get closer ",
         image: scene12,
-        audio: "/",
+        audio: "",
         progress: "67%",
         nextPage: "/safariPages/AR/2",
         previousPage: "/safariPages/AR/1",
@@ -149,7 +162,7 @@ const scenes = [
         id: 13,
         text: "Photographing the zebras was no easy task — but the lions were on a completely different level. Capturing them up close as they feasted on their prey… their heavy breaths from the hunt, mouths soaked in red.\n\nJakobsen spoke about the lions as if he knew them personally. He said they assigned roles — some scout, some hunt, some just watch.\n\nJust like us. We both kill.And yet I wonder — does the lion feel remorse, like I do?\n\nHow does Jakobsen feel? I had to know.",
         image: scene13,
-        audio: "/",
+        audio: "",
         progress: "71%",
         nextPage: "/safariPages/dialogue",
         previousPage: "/safariPages/AR/2",
@@ -159,20 +172,31 @@ const scenes = [
     {
         id: 14,
         text: "I used to think he did this for the thrill... the hunt... some twisted pride. But it’s not that. Not really. It’s something else. Something deeper.\n\nHe’s not just preserving bodies. He is trying to preserve wonder... trying to give it to someone who can’t be here to see it for himself. \n\nLook at his passion. His eyes... you can see his devotion.\n\nIt’s not just taxidermy.",
-        image: scene14,
-        audio: "/",
+        image: scene1415,
+        audio: "",
         progress: "90%",
-        nextPage: "/safariPages/notification/4",
+        nextPage: "/safariPages/scene/15",
         previousPage: "/safariPages/notification/3",
         characterImage: knud,
         buttonText: "Continue"
     },
     {
         id: 15,
+        text: "Hey, Knud! Come, help me with these bones. I still have some to sort before we go back to Denmark.",
+        image: scene1415,
+        audio: "",
+        progress: "95%",
+        nextPage: "/safariPages/notification/4",
+        previousPage: "/safariPages/scene/14",
+        characterImage: jakobsen,
+        buttonText: "Continue"
+    },
+    {
+        id: 16,
         text: "I still don’t know what we really brought back with us. Was it knowledge? Was it beauty? Or was it something we had no right to take? Maybe one day I’ll look at those photographs and feel proud. But right now... all I feel is the weight of it.",
-        image: scene15,
-        audio: "/",
-        progress: "00%",
+        image: scene16,
+        audio: "",
+        progress: "99%",
         nextPage: "/safariPages/sliderEnd",
         previousPage: "/safariPages/notification/4",
         characterImage: knud,
@@ -180,4 +204,3 @@ const scenes = [
 
     }
 ];
-export default scenes;
